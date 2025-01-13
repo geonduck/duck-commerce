1419669df15118c4d332096dc817e43408ee4f12
geonduck <geonduck93@gmail.com>
2025-01-13 22:14:47 +0900

    exception 발생 시 ErrorCode 설정 추가

--- src/main/java/kr/hhplus/be/server/config/http/CommonResponse.java
+++ src/main/java/kr/hhplus/be/server/config/http/CommonResponse.java
@@ -1,5 +1,7 @@
 package kr.hhplus.be.server.config.http;
 
+import kr.hhplus.be.server.config.exception.CommonException;
+import kr.hhplus.be.server.config.exception.ErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,9 +9,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CommonResponse {
 
-    @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<ApiResponse<Void>> handleIllegalStateException(IllegalStateException e) {
-        return ResponseEntity.badRequest().body(ApiResponse.error(e.getMessage()));
+    @ExceptionHandler(CommonException.class)
+    public ResponseEntity<ApiResponse<Void>> handleIllegalStateException(CommonException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(ApiResponse.error(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
